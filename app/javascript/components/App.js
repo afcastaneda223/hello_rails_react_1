f0b3f7b53ffc8e72cc7a2acb0e0c60f1b1011038
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-17 00:18:56 -0300

    update app & index

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HelloWorld from './HelloWorld'
+
 class App extends React.Component {
   render () {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<h1>hello</h1>} />
+          <Route path="/hello" element={<HelloWorld greeting="Friend"/>} />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
